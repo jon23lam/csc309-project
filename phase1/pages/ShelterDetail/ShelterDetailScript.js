f976e38533a1223680f8b45ca0854ad0b3fc9be7
ProEdge2 <63991231+ProEdge2@users.noreply.github.com>
2023-10-05 13:13:36 -0400

    alomost finished shelter detail page

--- phase1/pages/ShelterDetail/ShelterDetailScript.js
+++ phase1/pages/ShelterDetail/ShelterDetailScript.js
@@ -1,24 +1,32 @@
 function scrollToNextImage() {
-    const slider = document.querySelector('.slider');
-    const sliderWidth = slider.clientWidth;
-    const currentScrollLeft = slider.scrollLeft;
-    const nextScrollLeft = currentScrollLeft + sliderWidth;
+  const slider = document.querySelector(".slider");
+  const sliderWidth = slider.clientWidth;
+  const currentScrollLeft = slider.scrollLeft;
+  const nextScrollLeft = currentScrollLeft + sliderWidth;
 
-    // Check if we've reached the last image
-    if (nextScrollLeft >= slider.scrollWidth) {
-        // If so, smoothly scroll back to the first image
-        slider.scrollTo({
-            left: 0,
-            behavior: 'smooth'
-        });
-    } else {
-        // Otherwise, scroll to the next image with smooth behavior
-        slider.scrollTo({
-            left: nextScrollLeft,
-            behavior: 'smooth'
-        });
-    }
+  // Check if we've reached the last image
+  if (nextScrollLeft >= slider.scrollWidth) {
+    // If so, smoothly scroll back to the first image
+    slider.scrollTo({
+      left: 0,
+      behavior: "smooth",
+    });
+  } else {
+    // Otherwise, scroll to the next image with smooth behavior
+    slider.scrollTo({
+      left: nextScrollLeft,
+      behavior: "smooth",
+    });
+  }
 }
 
-setInterval(scrollToNextImage, 5000); // 5000 milliseconds = 5 seconds
+setInterval(scrollToNextImage, 8000); // 5000 milliseconds = 5 seconds
 
+const slider = document.querySelector(".slider");
+let currentIndex = 0;
+
+function moveSlider(direction) {
+  scrollToNextImage();
+  const translateX = -currentIndex * 100;
+  slider.style.transform = `translateX(${translateX}%)`;
+}
