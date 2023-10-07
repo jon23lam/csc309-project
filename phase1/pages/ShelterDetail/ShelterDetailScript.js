e14dec57a37537d6d20e098d5fd86af342e97295
ProEdge2 <63991231+ProEdge2@users.noreply.github.com>
2023-10-07 09:05:39 -0400

    fixed slider

--- phase1/pages/ShelterDetail/ShelterDetailScript.js
+++ phase1/pages/ShelterDetail/ShelterDetailScript.js
@@ -1,32 +1,30 @@
-function scrollToNextImage() {
-  const slider = document.querySelector(".slider");
-  const sliderWidth = slider.clientWidth;
-  const currentScrollLeft = slider.scrollLeft;
-  const nextScrollLeft = currentScrollLeft + sliderWidth;
-
-  // Check if we've reached the last image
-  if (nextScrollLeft >= slider.scrollWidth) {
-    // If so, smoothly scroll back to the first image
-    slider.scrollTo({
-      left: 0,
-      behavior: "smooth",
-    });
-  } else {
-    // Otherwise, scroll to the next image with smooth behavior
-    slider.scrollTo({
-      left: nextScrollLeft,
-      behavior: "smooth",
-    });
-  }
-}
-
-setInterval(scrollToNextImage, 8000); // 5000 milliseconds = 5 seconds
-
 const slider = document.querySelector(".slider");
+const prevButton = document.getElementById("prev-button");
+const nextButton = document.getElementById("next-button");
+
 let currentIndex = 0;
+let slideCount = slider.children.length;
+
+prevButton.addEventListener("click", () => {
+  moveSlider(-1);
+});
+
+nextButton.addEventListener("click", () => {
+  moveSlider(1);
+});
 
 function moveSlider(direction) {
-  scrollToNextImage();
-  const translateX = -currentIndex * 100;
-  slider.style.transform = `translateX(${translateX}%)`;
+  currentIndex += direction;
+
+  if (currentIndex < 0) {
+    currentIndex = slideCount - 1;
+  } else if (currentIndex >= slideCount) {
+    currentIndex = 0;
+  }
+
+  slider.children[currentIndex].scrollIntoView({
+    block: "nearest",
+    behavior: "smooth",
+    inline: "center",
+  });
 }
