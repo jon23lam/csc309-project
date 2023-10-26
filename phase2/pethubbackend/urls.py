2afc7c17315ff7cbf6ac7fdf26feba90655b4a5b
lewisp <lewisp@wingmateapp.com>
2023-10-25 20:41:14 -0400

    Add accounts app, base models for seeker and shelter users

--- phase2/pethubbackend/urls.py
+++ phase2/pethubbackend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
+API_PREFIX = 'api'
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(f'{API_PREFIX}/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path(f'{API_PREFIX}/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+
+    path(f'{API_PREFIX}/admin/', admin.site.urls),
+    path(f'{API_PREFIX}/accounts/', include('accounts.urls')),
 ]
