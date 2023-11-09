787d0244d6c302e7691f0fafa0973a06e18533b4
lewisp <lewisp@wingmateapp.com>
2023-11-09 12:22:48 -0500

    Make accounts/seeker and accounts/shelter urls mandatory for specifically finding a seeker or shelter. Also add status field to application model

--- petpal/accounts/urls.py
+++ petpal/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path(f'account/', views.AccountRegistrationView.as_view()),
-    path(f'account/<int:pk>/', views.AccountView.as_view()),
+    path(f'shelter/<int:pk>/', views.ShelterView.as_view()),
+    path(f'seeker/<int:pk>/', views.SeekerView.as_view()),
     path(f'shelters/', views.SheltersListView.as_view()),
 ]
