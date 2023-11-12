74ae4be2696342d63e022fc57405b509086db891
Jon Lam <jonl@wingmateapp.com>
2023-11-12 17:47:47 -0500

    adding serializers

--- petpal/petlistings/serializers.py
+++ petpal/petlistings/serializers.py
@@ -5,4 +5,4 @@ from .models import PetListing
 class PetListingSerializer(serializers.ModelSerializer):
     class Meta:
         model = PetListing
-        fields = ('lister', 'name', 'animal', 'breed', 'sex', 'province', 'address', 'colour', 'age', 'weight', 'description', 'status')
+        fields = ('id', 'image', 'lister', 'name', 'animal', 'breed', 'sex', 'province', 'address', 'colour', 'age', 'weight', 'description', 'status')
