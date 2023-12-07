2f19fc9363f1878ef158b447bcca4c1ac482bd52
Jacob DeSousa <jacobd2009@hotmail.com>
2023-12-06 19:10:38 -0500

    feat: Add signup pages

--- phase3/frontend/src/App.js
+++ phase3/frontend/src/App.js
@@ -12,6 +12,8 @@ import { HeaderRoute } from "./routes/HeaderRoute"
 
 import "./App.scss";
 import { PetDetailPage } from "./pages/petseeker/Detail/PetDetail/PetDetailPage";
+import {SeekerSignup} from "./pages/signup/Seeker/Seeker";
+import {ShelterSignup} from "./pages/signup/Shelter/Shelter";
 
 function App() {
   return (
@@ -22,6 +24,8 @@ function App() {
             <RootProvider>
               <Routes>
                 <Route path="/login" element={<Login />} />
+                <Route path="/signup/seeker" element={<SeekerSignup />} />
+                <Route path="/signup/shelter" element={<ShelterSignup />} />
 
                 {/* Put all routes that need authentication in here */}
                 <Route path="/" element={<AuthenticatedRoute />}>
@@ -30,7 +34,7 @@ function App() {
                   <Route path="/application/:id/" element={<Application />} />
                   <Route
                     path="/editlisting/:id/"
-                    element={<PetListingEditor />}
+                    element={<PetListingEditor />} />
                   <Route path="/petdetail/:id/" element={<PetDetailPage />} />
                   <Route path="/manage_shelter" element={<ShelterManagement />} />
                   <Route path="/createlisting" element={<PetListingEditor />} />
---
+++ phase3/frontend/src/requests/signup.js
@@ -0,0 +1,8 @@
+import {axiosPostNoAuth} from "./axiosRequests"
+import {endpoint} from "../utils/endpoint";
+
+const SIGNUP_ENDPOINT = endpoint("api/accounts/account/");
+
+export async function signup(request) {
+    return await axiosPostNoAuth(SIGNUP_ENDPOINT, request);
+}
