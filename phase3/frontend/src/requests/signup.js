ff9aa2ff325986dcdeda5eb05d59dc0d3d38ace0
Jacob DeSousa <jacobd2009@hotmail.com>
2023-12-06 20:12:55 -0500

    feat: Fix content type

--- phase3/frontend/src/requests/signup.js
+++ phase3/frontend/src/requests/signup.js
@@ -4,5 +4,8 @@ import {endpoint} from "../utils/endpoint";
 const SIGNUP_ENDPOINT = endpoint("api/accounts/account/");
 
 export async function signup(request) {
-    return await axiosPostNoAuth(SIGNUP_ENDPOINT, request);
+    return await axiosPostNoAuth(SIGNUP_ENDPOINT, request,
+        {
+            "Content-Type": "multipart/form-data",
+    });
 }
