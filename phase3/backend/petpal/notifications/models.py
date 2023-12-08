e4d5a2e00f79135f5b5a565c04401c9fa2448270
lewisp <lewisp@wingmateapp.com>
2023-12-08 13:37:55 -0500

    Notifications should now be automatically generated when comments, applications, or application updates are made

--- phase3/backend/petpal/notifications/models.py
+++ phase3/backend/petpal/notifications/models.py
@@ -11,9 +11,8 @@ class Notification(models.Model):
   read = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
   type = models.CharField(max_length=30, choices=(('application_comment', 'Application Comment'), ('shelter_comment', 'Shelter Comment'), ('application', 'Application'), ('status_update', "Status Update")))
-  associated_id = models.IntegerField()
-  application_id = models.IntegerField(blank=True, null=True)
-  shelter_id = models.IntegerField(blank=True, null=True)
+  application = models.ForeignKey('applications.Application', on_delete=models.CASCADE, blank=True, null=True)
+  comment = models.ForeignKey('comments.Comment', on_delete=models.CASCADE, blank=True, null=True)
 
   class Meta:
     ordering = ['-created_at']
